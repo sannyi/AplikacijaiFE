8ff40778f51f25e2fc891d6d417663c3dd7eed6a
ak3900LMMFE <aleksander.kovac97@hotmail.com>
2017-05-27 13:56:56 +0200

    ASP projekt dodan
    ASP projekt dodan

---
+++ Aplikacija iFE/Aplikacija za dostop do podatkov/ConnectToSqlServer/ConnectToSqlServer/Models/Skoda.cs
@@ -0,0 +1,17 @@
+﻿
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace ConnectToSqlServer.Models
+{
+    [Table("Skoda")]
+    public class Skoda
+    {
+        
+        public string Opis { get; set; }
+        public string Lokacija { get; set;}
+        public string PotDoSlikeSkode { get; set; }
+        
+        public int IDprijaviteljaSkode { get; set; }
+    }
+}
